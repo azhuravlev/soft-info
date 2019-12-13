9e214aa1da16759ff1cbed263b5bdd6dfb253ec9
Alexey Zhuravlev <alexey.g.zhuravlev@gmail.com>
2019-12-13 17:02:59 +0300

    Add sendgrid notifications

--- broker/app/models/tool.rb
+++ broker/app/models/tool.rb
@@ -1,4 +1,17 @@
 class Tool < ApplicationRecord
   validates :name, presence: true
   validates :repo_url, presence: true
+
+  after_create :notify_add
+  after_update :notify_changes
+
+  private
+
+  def notify_add
+    SendgridClient.new.notify_add(self)
+  end
+
+  def notify_changes
+    SendgridClient.new.notify_changes(self)
+  end
 end
